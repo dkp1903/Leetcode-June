564cd220432946914ca9cce222710008509425fe
Dushyant Pathak <dushyantpathak6789@gmail.com>
2020-06-05 23:46:49 +0530

    Create RevereString.cpp

---
+++ RevereString.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int low = 0;
+        int high = s.size()-1;
+        char temp;
+        while(low < high)
+        {
+            temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low++;
+            high--;
+        }
+
+    }
+};
